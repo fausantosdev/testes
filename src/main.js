5d2174af955dbc7d507712d19cbb0a102f39fa23
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-09-10 00:49:54 +0000

    :construction: restructure folders

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '../styles/style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Testes</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
